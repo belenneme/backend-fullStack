a4c1477c233cfad5c8da56bde06f46602ba2f394
Belen Neme <belenneme1@gmail.com>
2020-12-02 22:07:10 -0300

    First Commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require('./config/config')
+const express = require("express");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+app.use(bodyParser.json())
+
+app.use(require('./rutas/usuario'))
+
+mongoose.connect('mongodb://localhost:27017/rolling', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+},(err, res)=>{
+  if(err) throw err;
+  console.log('Base de datos online')
+
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Genial! estamos online:", process.env.PORT);
+});
